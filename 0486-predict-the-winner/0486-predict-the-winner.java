146ae007c1bfd7c6229ea5880c6845e3266f6992
samarth bhardwaj <bhardwajsamarth122@gmail.com>
2026-07-01 04:09:17 +0530

    Time: 795 ms (5.11%), Space: 279.9 MB (5.41%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,35 +1,42 @@
+
 class Solution {
 
-    public boolean solve(int[] nums, int i, int j, int chance, int p1, int p2){
-        if(i > j){
-            if(p1 >= p2){
-                return true;
-            }
-            return false;
+    HashMap<String, Boolean> map = new HashMap<>();
+
+    public boolean solve(int[] nums, int i, int j, int chance, int p1, int p2) {
+
+        if (i > j) {
+            return p1 >= p2;
+        }
+
+        String key = i + "#" + j + "#" + chance + "#" + p1 + "#" + p2;
+
+        if (map.containsKey(key)) {
+            return map.get(key);
         }
 
         boolean check = false;
-        if(chance == 0){
-            boolean f1 = solve(nums, i + 1, j, 1 - chance, p1 + nums[i], p2);
-            boolean f2 = solve(nums, i, j - 1, 1 - chance, p1 + nums[j], p2);
 
-            if(f1 || f2){
-                check = true;
-            }
-        }else{
-            boolean f1 = solve(nums, i + 1, j, 1 - chance, p1, p2 + nums[i]);
-            boolean f2 = solve(nums, i, j - 1, 1 - chance, p1, p2 + nums[j]);
+        if (chance == 0) {
 
-            if(f1 && f2){
-                check = true;
-            }
+            boolean left = solve(nums, i + 1, j, 1, p1 + nums[i], p2);
+            boolean right = solve(nums, i, j - 1, 1, p1 + nums[j], p2);
+
+            check = left || right;
+
+        } else {
+
+            boolean left = solve(nums, i + 1, j, 0, p1, p2 + nums[i]);
+            boolean right = solve(nums, i, j - 1, 0, p1, p2 + nums[j]);
+
+            check = left && right;
         }
 
+        map.put(key, check);
         return check;
     }
 
     public boolean predictTheWinner(int[] nums) {
-        int n = nums.length;
-        return solve(nums, 0, n-1, 0, 0, 0);
+        return solve(nums, 0, nums.length - 1, 0, 0, 0);
     }
 }
