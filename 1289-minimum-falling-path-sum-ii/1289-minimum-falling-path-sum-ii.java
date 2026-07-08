5a974a019656aa0d86b7ac7c745bd1c3b1fcf601
samarth bhardwaj <bhardwajsamarth122@gmail.com>
2026-07-08 19:03:57 +0530

    Time: 5 ms (76.08%), Space: 55.4 MB (9.07%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -8,22 +8,40 @@ class Solution {
         }
 
         int ans = Integer.MAX_VALUE;
+        int min1_ = Integer.MAX_VALUE;
+        int min2_ = Integer.MAX_VALUE;
 
         for(int i = n-1; i >= 0; i--){
+            int min1 = Integer.MAX_VALUE;
+            int min2 = Integer.MAX_VALUE;
             for(int j = n-1; j >= 0; j--){
                 if(i == n-1){
                     dp[i][j] = grid[i][j];
-                    continue;
+                    
+                    //continue;
                 }
-                for(int col = 0; col < n; col++){
-                    if(j != col){
-                        dp[i][j] = Math.min(dp[i][j], grid[i][j] + dp[i+1][col]);
+
+                else if(dp[i+1][j] != min1_){
+                    dp[i][j] = grid[i][j] + min1_;
+                }
+                else{
+                    dp[i][j] = grid[i][j] + min2_;
+                }
+
+
+                if(min1 > dp[i][j]){
+                        min2 = min1;
+                        min1 = dp[i][j];
                     }
-                }
+                    else if(min2 > dp[i][j]){
+                        min2 = dp[i][j];
+                    }
                 if(i == 0){
                     ans = Math.min(ans, dp[i][j]);
                 }
             }
+            min1_ = min1;
+            min2_ = min2;
         }
 
         return ans;
