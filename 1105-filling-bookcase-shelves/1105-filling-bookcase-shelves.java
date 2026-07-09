9e1282642e6f1a12e8be7bad6f146b8cfb1f8906
samarth bhardwaj <bhardwajsamarth122@gmail.com>
2026-07-09 17:52:28 +0530

    Time: 2 ms (24.49%), Space: 49.3 MB (27.35%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
@@ -1,27 +1,28 @@
 class Solution {
     int n;
     int sw;
-    Map<String, Integer> map;
+    Integer dp[][];
     public int solve(int i, int width, int height,int[][] books){
         if(i == n){
             return height;
         }
-        String key = "" + i + "," + width + "," + height;
-        if(map.containsKey(key)) return map.get(key);
+        if(dp[i][width]!= null) return dp[i][width];
+
         int same = Integer.MAX_VALUE;
         if(books[i][0] + width <= sw){
             same = solve(i+1, books[i][0] + width, Math.max(height, books[i][1]), books);
         }
         int next = height + solve(i+1, books[i][0], books[i][1], books);
+
         int ans = Math.min(next, same);
-        map.put(key, ans);
+        dp[i][width] = ans;
         return ans;
     }
 
     public int minHeightShelves(int[][] books, int shelfWidth) {
         n = books.length;
         sw = shelfWidth;
-        map = new HashMap<>();
+        dp = new Integer[n+1][1001];
         return solve(0, 0, 0, books);
     }
 }
