77bc3c1cddd96dc7356aedad82f5c3f4aaa51a7c
samarth bhardwaj <bhardwajsamarth122@gmail.com>
2026-07-12 15:03:12 +0530

    Time: 216 ms (18.48%), Space: 109.4 MB (35.7%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
@@ -1,38 +1,32 @@
 class Solution {
     Map<Integer, Integer> map;
-    Integer[][] dp;
-
-    public int solve(int j, int k, int[] arr){
-        if(dp[j][k] != null) return dp[j][k];
-        int val = arr[k] - arr[j];
-
-        if(map.containsKey(val) && map.get(val) < j){
-            int i = map.get(val);
-            return dp[j][k] = solve(i, j, arr) + 1;
-        }
-        
-        return dp[j][k] = 2;
-    }
+    int[][] dp;
 
     public int lenLongestFibSubseq(int[] arr) {
         int n = arr.length;
-        dp = new Integer[n+1][n+1];
+        dp = new int[n][n];
         map = new HashMap<>();
-        for(int i = 0; i< n; i++){
+        for (int i = 0; i < n; i++) {
             map.put(arr[i], i);
         }
-        int ans = Integer.MIN_VALUE;
-        for(int j = 1; j < n; j++){
-            for(int k = j+1; k < n; k++){
-                int length = solve(j, k, arr);
+         for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], 2);   // Base length of every pair
+        }
 
-                if(length >= 3){
-                    ans = Math.max(ans, length);
+        int ans = Integer.MIN_VALUE;
+        for (int j = 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                int val = arr[k] - arr[j];
+                if (map.containsKey(val) && map.get(val) < j) {
+                    int i = map.get(val);
+                    dp[j][k] = dp[i][j] + 1;
+
                 }
+                ans = Math.max(dp[j][k], ans);
             }
 
         }
 
-        return ans == Integer.MIN_VALUE ? 0 : ans;
+        return ans < 3 ? 0 : ans;
     }
 }
