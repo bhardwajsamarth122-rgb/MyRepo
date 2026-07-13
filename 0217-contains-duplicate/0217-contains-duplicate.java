487bee7b6131db2016cdeaf1b04c49ba1b227d4d
samarth bhardwaj <bhardwajsamarth122@gmail.com>
2026-07-13 18:55:50 +0530

    Time: 33 ms (5.01%), Space: 107.1 MB (21.21%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if(map.get(nums[i]) > 1) return true;
+        }
+        return false;
+    }
+}
